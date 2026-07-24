2f1cb07f4602e6bd17cfffa13d36c530bd438fa1
407-21-Kaenika <54515@yupparaj.ac.th>
2026-07-24 11:29:03 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -4,7 +4,7 @@ st.title("🛒แอปพลิเคชั่นคำนวณราคาส
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
 vat = price * 0.07
 net_price = price - vat
-vat = price * 0.07
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
+st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
 st.divider()
 st.write("นางสาวเกณิกา พูลทะจักร์ เลขที่ 21  ม.4/7")
