4b0ddcfae7e1ebb685c916f04b09eb9fcf652fb8
407-21-Kaenika <54515@yupparaj.ac.th>
2026-08-21 00:18:40 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -129,7 +129,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
-def show_result_dialog(ans1, ans2, ans3, ans4)
+    def show_result_dialog(ans1, ans2, ans3, ans4)
 
 st.divider()
 st.write("นางสาวเกณิกา พูลทะจักร์ เลขที่ 21 ม.4/7")
